09d7e4473019f3df94e117b66c4a51556c1b3fff
amedeo <amedeopasanisi97@gmail.com>
2021-07-25 00:03:24 +0200

    fine pari e dispari

--- pari e dispari/js/main.js
+++ pari e dispari/js/main.js
@@ -1,6 +1,34 @@
 // Pari e Dispari
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
+function rndNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+function pariDispari (num) {
+    if (num % 2 == 0) {
+        return "pari";
+    }
+    return "dispari";
+}
+
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+var utente = prompt("inserisci 'pari' o 'dispari'");
+var numeroUtente = parseInt(prompt("inserisci un numero tra 1 e 5 inclusi"));
+var numeroComputer = rndNum(1, 5);
+console.log("hai scelto " + utente)
+console.log("numero utente: " + numeroUtente);
+console.log("numero computer: " + numeroComputer);
+
+// Sommiamo i due numeri
+var somma = numeroUtente + numeroComputer;
+console.log("somma: " + somma);
+console.log(somma + " è un numro " + pariDispari(somma));
+
 // Dichiariamo chi ha vinto.
+if (pariDispari(somma) == utente) {
+    console.log("hai vinto!");
+} else {
+    console.log("ha vinto il computer :(");
+}
