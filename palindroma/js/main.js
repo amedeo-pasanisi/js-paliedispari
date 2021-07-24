8493a12c47e4dae0d46d6999addac83b9cf9698f
amedeo <amedeopasanisi97@gmail.com>
2021-07-24 23:21:01 +0200

    fine palindroma

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,3 +1,17 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+var parola = prompt("inserisci una parola");
+
+function palindroma (string) {
+    var reverseString = string.toLowerCase().split('').reverse().join('');
+    console.log(reverseString)
+    
+    if (string === reverseString) {
+        return "parola palindroma"
+    }
+    return "parola non palindroma"
+}
+
+console.log(palindroma(parola));
